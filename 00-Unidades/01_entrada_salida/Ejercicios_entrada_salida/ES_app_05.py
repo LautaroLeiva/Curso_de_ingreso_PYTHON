15a9791c685bede9ebd8de623cdfec1e727dfa53
LautaroLeiva <lautarogabriel_leiva@yahoo.com.ar>
2024-02-06 22:27:42 -0300

    actividad 1,2,3,4,5 hechas

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Lautaro 
+apellido: Leiva
 ---
 Ejercicio: entrada_salida_05
 ---
@@ -40,7 +40,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre_persona = self.txt_nombre.get()
+        edad_persona = self.txt_edad.get()
+        alert("Identificación", f"Tu nombre es {nombre_persona} y tu edad es de {edad_persona} años")
 
         
     
